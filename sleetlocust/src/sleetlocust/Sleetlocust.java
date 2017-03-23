a3d9c00fe08888b4944bdc86f129d23fed056f85
Seb Rupik <sebrupik@gmail.com>
2017-03-23 23:26:18 +0000

    it's a start

---
+++ sleetlocust/src/sleetlocust/Sleetlocust.java
@@ -0,0 +1,21 @@
+/*
+ * SNMP agent which recives (intercepts), encapsulates and forwards SNMP GET messages
+ * and forwards them to our SNMP cache server.
+ * The response will be sent back to the agent and source address spoofed.
+ */
+package sleetlocust;
+
+/**
+ *
+ * @author snr
+ */
+public class Sleetlocust {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
